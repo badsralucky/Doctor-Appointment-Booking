51f681568e7d9b4a37042033551e1bf28b8c79a1
badsralucky <92315225+badsralucky@users.noreply.github.com>
2026-04-03 23:55:00 +0530

    Delete Routes directory

--- Routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { SignUp ,login } from "../Controllers/authController.js"
-
-const router = express.Router()
-
-router.post('/SignUp' , SignUp)
-router.post('/login' , login)
-
-export default router
