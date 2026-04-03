910bb1fb96680ffaa186c0ce072eb9dd4a6c7dd6
badsralucky <92315225+badsralucky@users.noreply.github.com>
2026-04-04 00:10:45 +0530

    Add files via upload

---
+++ Frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. George will",
+    specialization: "Surgeon",
+    avgRating: 4.2,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 1900,
+    hospital: " VJ Hospital, Delhi",
+  },
+  {
+    id: "02",
+    name: "Dr.Ankush Agarwal",
+    specialization: "Neurologist",
+    avgRating: 4.5,
+    totalRating: 358,
+    photo: doctorImg02,
+    totalPatients: 1000,
+    hospital: " VJ Hospital, Delhi",
+  },
+  {
+    id: "03",
+    name: "Dr. Mohit Sharan",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 291,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "  VJ  Hospital, Delhi",
+  },
+];
